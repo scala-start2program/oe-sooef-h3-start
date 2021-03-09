813abd70a2d1988e6ec3c568af7588c6c69cca84
jandedeuraerder-howest <jan.de.deurwaerder@howest.be>
2021-03-09 13:45:38 +0100

    Add project files.

---
+++ Scala.Oefening03.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace Scala.Oefening03.Wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void lstVloot_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+        }
+
+        private void btnNew_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void btnEdit_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+        }
+    }
+}
